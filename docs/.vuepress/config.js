9d5a18487fbd5346422a5d3ae8d6a8f62229e103
SongYu <songyu2049704879@gmail.com>
2024-02-02 00:18:20 +0800

    1

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'TypeScript4 文档',
+    description: 'TypeScript4 最新官方文档翻译',
+    base: '/gpt-blog/',
+    theme: 'reco',
+    locales: {
+        '/': {
+            lang: 'zh-CN'
+        }
+    },
+}
