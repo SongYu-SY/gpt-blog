8a1ac657548ea9cca1ba83f1aa3761fd1c7092b9
SongYu <songyu2049704879@gmail.com>
2024-02-02 01:18:18 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    title: 'ChatGPT Plus',
-    description: 'ChatGPT Plus',
+    title: 'ChatGPT-Plus',
+    description: '2024年升级 ChatGPT-Plus 的教程（虚拟信用卡升级 GPT4.0 ）',
     base: '/gpt-blog/',
     theme: 'reco',
     locales: {
